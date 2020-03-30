6c1b533ed7d601570b6b0be60afc540e2dced00c
LevanceWam <lkwamley@fullsail.edu>
2020-03-29 23:00:17 -0400

    learned inputs and completed 2 exercises

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,4 +1,33 @@
 print("Hello world Vance is here")
 print('o-------')
 print(' |||||')
+# this way we can have 10 * instead of having to type it out
 print('*' * 10)
+
+# price will change because it goes from top to bottom and it goes from the last declared so it will print 20
+price = 10
+price = 20
+rating = 4.9
+name = 'vance'
+is_published = False
+print(price)
+
+
+# Create a new patient named john smith 20 years old and if he is a new patient.
+
+firstName = 'John'
+lastName = 'Smith'
+age = 20
+new_patient = True
+
+print('Patient Info: ', firstName, lastName, age, 'is a new patient', new_patient)
+
+# input is python's way of asking questions
+new_name = input('What is your name? ')
+print('Hi ' + new_name)
+
+# Create 2 prompts asking the user's name and favorite color
+
+ask_name = input('What is your name buddy? ')
+ask_color = input ('What is your favorite color guy? ')
+print(ask_name + ' loves ' + ask_color)
