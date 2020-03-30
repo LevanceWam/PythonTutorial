7ccd4f8d3a7123d66373a63db3f68759bc244f52
LevanceWam <lkwamley@fullsail.edu>
2020-03-30 16:18:24 -0400

    finished while loops

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -237,3 +237,24 @@ elif 'l' in unit or "L" in unit:
     print(f'You are {lbs_weight}lbs')
 else:
     print(f' You entered {ask_weight} but forgot to chose a conversion')
+
+# While Loops
+i = 1
+while i <= 5:
+    print(i)
+    print('*' * i)
+    i = i + 1
+print('Done')
+
+# guessing game
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input('Guess: '))
+    guess_count += 1
+    if guess == secret_number:
+        print('You won!')
+        break
+else:
+    print('Sorry you failed')
