da565b83cd759c4ab9cba4422e27f45a232c4edb
LevanceWam <lkwamley@fullsail.edu>
2020-03-30 00:52:56 -0400

    Finished strings going in to next

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -50,3 +50,40 @@ print(type(kilogram))
 print(' Your converted weight is: ', kilogram, 'kg')
 
 # Strings
+# strings for multiple length example tripe quotes
+course = '''
+Hi Vance, 
+
+Here is our first email to you.
+
+Thank you,
+The Support Team
+
+'''
+
+course2 = 'Python for Beginners'
+# indexes do not start with 1 they start with 0 so P will be 0
+print(course2[0])
+# this will go backwards from the start of the index
+print(course2[-2])
+# this way we can call more than one index we are going to go from 0 - 4 but it will only show up to the third index
+print(course2[0:4])
+# By only using 1 number and leaving the other blank we will take away characters in the index so if we type in 2
+# indexes 0,1 will be missing
+print(course2[2:])
+# if we do it from the opposite side of the colon it will start from 0 to the number you chose in the index
+print(course2[:6])
+
+# cloning the course2 variable
+another = course2[:]
+print(another)
+
+# test what would happen if we printed a variable with this [1:-1] I believe index 0 will only show up
+new_name = 'Chelsea'
+print(new_name[1:-1])
+# I was wrong it took out the first and last indexes
+
+# Formatted Strings
+
+
+
