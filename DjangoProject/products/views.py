7dbffbdd8741a8f888a30dd2a0a5bf31a7180336
LevanceWam <lkwamley@fullsail.edu>
2020-04-14 15:09:47 -0400

    completed my first exercise in django

---
+++ DjangoProject/products/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Exercise Create a new path to a page called new
+def index(request):
+    return HttpResponse('Hello World')
+
+
+def new(request):
+    return HttpResponse('Welcome this a the new item page')
