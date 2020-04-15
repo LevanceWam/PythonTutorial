648d8d442c23b18f81ca917c0dd19a5b79a7cf51
LevanceWam <lkwamley@fullsail.edu>
2020-04-14 20:16:04 -0400

    Finished customizing items from the admins view

--- DjangoProject/products/admin.py
+++ DjangoProject/products/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Offer
 
-admin.site.register(Product)
+
+# This is what we need to add so when whe are on the admin's page we can see the stock by name
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
